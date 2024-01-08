5ee203fb75b9920a2d55ed4b8f09b29b3b846cea
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-09 02:50:00 +0530

    Add files via upload

---
+++ E_nestedLoop.js
@@ -0,0 +1,11 @@
+//nested loop
+for(person=1;person<=5;person++)
+{
+    console.log("Welcome person "+person)
+    //feed the 3 food items to person 1
+    for(food=1;food<=3;food++)
+    {
+        console.log("Feeding food item "+food)
+    }
+}
+//predict the output of the nested loop
