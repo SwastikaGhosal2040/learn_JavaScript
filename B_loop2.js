7ec39517f27342201ef1a256c89d62a2510b9bd8
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-09 00:22:18 +0530

    Add files via upload

---
+++ B_loop2.js
@@ -0,0 +1,28 @@
+//for loops
+for(i=10;i>0;i--)
+{
+    console.log(i)
+    console.log("Hello Students!")
+}
+/*what should be the output?
+10
+Hello Students!
+9
+Hello Students!
+8
+Hello Students!
+7
+Hello Students!
+6
+Hello Students!
+5
+Hello Students!
+4
+Hello Students!
+3
+Hello Students!
+2
+Hello Students!
+1
+Hello Students!
+*/
