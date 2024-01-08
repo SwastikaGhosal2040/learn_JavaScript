045adcf53750a341085c23a01d8782a8c65ea48d
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-09 01:42:53 +0530

    Add files via upload

---
+++ B_loop3.js
@@ -0,0 +1,7 @@
+for(i=0,j=10;i<j;i++,j--)
+{
+    console.log("Hello Students!,i,j:"+i+","+j)
+}
+/*Predict the output when more than 1 initialization is present
+and increment, decrement both are given.
+*/ 
