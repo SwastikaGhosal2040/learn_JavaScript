f2f5b835bcc5496773b1a4d776ae7ddd469e6e52
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-09 00:08:33 +0530

    Add files via upload

---
+++ C2_loopTest.js
@@ -0,0 +1,7 @@
+//what will happen if the condition is absent in the for loop?
+for(i=1;;i++){
+    console.log("Hello Students!")
+}
+/*It will print an infinite loop.
+As nothing is in the condition, the default condition is true.
+Condition always results boolean.*/
