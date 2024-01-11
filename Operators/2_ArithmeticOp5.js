ad0cd29871540d030a4f4dc90e1f5e0f1ede6da2
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-12 04:43:31 +0530

    Add files via upload

---
+++ Operators/2_ArithmeticOp5.js
@@ -0,0 +1,9 @@
+//predict the output
+num1 = 10
+num2 = 20
+
+var remainder = num2 % num1
+
+console.log("1st num is: ",num1)
+console.log("2nd num is: ",num2)
+console.log("Remainder is: ",remainder)
