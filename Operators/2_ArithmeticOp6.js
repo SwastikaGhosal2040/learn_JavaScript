665aec135cef861c4318eac8ac122f1b5c22527e
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-12 05:08:00 +0530

    Add files via upload

---
+++ Operators/2_ArithmeticOp6.js
@@ -0,0 +1,9 @@
+//predict the output
+num1 = 10
+num2 = 20
+
+var exponent = num2 ** num1
+
+console.log("1st num is: ",num1)
+console.log("2nd num is: ",num2)
+console.log(num2," to the power ",num1," is:",exponent)
