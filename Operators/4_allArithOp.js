195a52dd0aa2c4575d005499debd5e42967125e8
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-14 02:55:41 +0530

    Add files via upload

---
+++ Operators/4_allArithOp.js
@@ -0,0 +1,21 @@
+//predict output:
+x = 5 
+x+=9
+console.log(x)
+x-=7
+console.log(x)
+x*=2
+console.log(x)
+x**=2
+console.log(x)
+x/=3
+console.log(x)
+
+/**
+ * Output:
+ * 14
+   7
+   14
+   196
+   65.33333333333333
+ */
