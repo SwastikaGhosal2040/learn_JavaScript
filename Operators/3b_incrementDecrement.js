c98c0ee1d33d234dfedb88aeb7c6983a95bc72b6
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-14 02:33:52 +0530

    Add files via upload

---
+++ Operators/3b_incrementDecrement.js
@@ -0,0 +1,21 @@
+//predict the output:
+a = 5
+b = 5
+console.log(a++)
+console.log(++b)
+
+console.log(a)
+console.log(b)
+
+console.log(--a)
+console.log(a--)
+
+/**
+ * Output:
+ *  5
+    6
+    6
+    6
+    5
+    5
+ */
