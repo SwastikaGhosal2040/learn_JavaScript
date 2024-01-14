0b20837f1a13a4754746165bf76a008fde03d4dd
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-14 06:45:17 +0530

    Add files via upload

---
+++ Operators/6_RelationalOp2b.js
@@ -0,0 +1,18 @@
+num1 = 50
+num2 = '50'
+
+console.log(num1 == num2)
+console.log(num1 === num2)
+
+/**
+ * num1 == num2-> Equality-> Equality operator compares value only
+ * console.log(num1 == num2)->value equal
+ * returns true
+ * num1 === num2->Strict Equality compares type and value both
+ * console.log(num1 === num2)->type different, value same.
+ * returns false
+ * 
+ * Output:
+ * true
+ * false
+ */
