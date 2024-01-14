3266afded73f2476d667c51d3ce7772ee8a515c5
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-14 05:43:33 +0530

    Add files via upload

---
+++ Operators/6_RelationalOp2.js
@@ -0,0 +1,14 @@
+//Strict Equality
+num1 = 50
+num2 = 57
+console.log(num1===num2)
+
+/**
+ * Strict Equality compares type and value both
+ * Here type same int, value different.
+ * num1===num2 ->condition false
+ * 
+ * Output:
+ * false
+ * 
+ */
