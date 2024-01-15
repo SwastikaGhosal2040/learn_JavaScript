986a6e6df4552f196338fe7ce2172f75839a4d45
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-16 01:23:49 +0530

    Add files via upload

---
+++ Operators/b_UpperCase.js
@@ -0,0 +1,11 @@
+name = "Swastika"
+console.log(name)
+console.log(name.toUpperCase())
+
+/**
+ * Example:
+ * age = 23
+ * console.log(age.toUpperCase())
+ * -> Error
+ * Because toUpperCase() can only applied with strings
+ */
