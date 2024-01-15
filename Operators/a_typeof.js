2fc460c4e488bed92aa97bbf634987e599dbe125
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-16 01:22:14 +0530

    Add files via upload

---
+++ Operators/a_typeof.js
@@ -0,0 +1,13 @@
+name = "Swastika"
+age = 19
+isAlive = true
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isAlive)
+
+/**
+ * Output:
+ * string
+ * number
+ * boolean
+ */
