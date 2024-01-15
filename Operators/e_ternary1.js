5248adf59922741c1d4054244fdec869fa1629e3
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-16 01:30:32 +0530

    Add files via upload

---
+++ Operators/e_ternary1.js
@@ -0,0 +1,11 @@
+isTheUserLoggedIn = false
+isTheCoursePurchased = false
+ 
+isTheUserLoggedIn ?
+isTheCoursePurchased ? console.log('Access Granted') : 
+console.log('Please buy') : console.log('Please login')
+
+/**
+ * Output:
+ * Please login
+ */
