f4b463787991ee600240c9a5719617f75b0bad18
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-17 02:14:50 +0530

    Add files via upload

---
+++ Conditionals/2_bool2.js
@@ -0,0 +1,14 @@
+isAdmin = true
+if(isAdmin)
+{
+    console.log("Fetching the user details")
+}
+else
+{
+    console.log("Not able to fetch the details as am not admin")
+}
+
+/**
+ * Output:
+ * Fetching the user details
+ */
