06ffef483d3fa23bb6e87742c5d834ed3a453010
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-17 02:29:35 +0530

    Add files via upload

---
+++ Conditionals/3c_divBy2or3or5.js
@@ -0,0 +1,23 @@
+//predict the output:
+num = 25
+if(num % 2 == 0)
+{
+    console.log("Number is divisible by 2")
+}
+else if (num % 3 == 0)
+{
+    console.log("Number is divisible by 3") 
+}
+else if (num % 5 == 0)
+{
+    console.log("Number is divisible by 5")
+}
+else
+{
+    console.log("Number can't be divided by 2 or 3 or 5")
+}
+
+/**
+ * Output:
+ * Number is divisible by 5
+ */
