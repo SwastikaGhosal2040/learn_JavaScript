56c32dad5f44db64f8976c9a0af9e1da4ad9c8e0
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-17 01:49:53 +0530

    Add files via upload

---
+++ Conditionals/1_if1.js
@@ -0,0 +1,15 @@
+a = 25
+b =  5
+if(b != 0)
+{
+    console.log("Inside if block")
+    console.log(a/b)
+}
+console.log("After if block")
+
+/**
+ * Output:
+ * Inside if block
+   5
+   After if block
+ */
