fa00b375003dbe5918343dcdadab6b73c8bf3538
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-17 02:16:17 +0530

    Add files via upload

---
+++ Conditionals/2_evenOdd.js
@@ -0,0 +1,16 @@
+num = 24
+if(num % 2 == 0)
+{
+    console.log("Inside if, the number is even")
+}
+else
+{
+    console.log("Inside else, number is odd")
+}
+console.log("After conditionals")
+
+/**
+ * Output:
+ * Inside if, the number is even
+   After conditionals
+ */
