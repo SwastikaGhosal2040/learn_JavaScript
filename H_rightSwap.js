86ffc593607b2e095e61147682c13682e7075058
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-19 23:23:42 +0530

    Add files via upload

---
+++ H_rightSwap.js
@@ -0,0 +1,17 @@
+//predict output:
+right_swaps = 10
+while(right_swaps >= 0)
+{
+    if(right_swaps==3||right_swaps==7||right_swaps==9)
+    {
+        continue
+    }
+    console.log("Keep swiping right")
+    right_swaps = right_swaps - 1
+}
+
+/**
+ * Output:
+ * Keep swiping right
+ */
+
