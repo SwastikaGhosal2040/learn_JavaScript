89cede3039e8c8d035e53847dee0867fe0fe9322
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-26 13:11:22 +0530

    Add files via upload

---
+++ Conditionals/4d_findDay.js
@@ -0,0 +1,41 @@
+//predict the output:
+//1->Monday , 7->Sunday
+day = 3
+
+switch(day)
+{
+    case 1:
+        console.log("Monday")
+        
+    case 2:
+        console.log("Tuesday")
+        
+    case 3:
+        console.log("Wednesday")
+        
+    case 4:
+        console.log("Thursday")
+        
+    case 5:
+        console.log("Friday")
+        
+    case 6:
+        console.log("Saturday")
+        
+    case 7:
+        console.log("Sunday")
+        
+    default:
+        console.log("Not a valid number")
+    
+}
+
+/**
+ * Output:
+ * Wednesday
+ * Thursday
+ * Friday
+ * Saturday
+ * Sunday
+ */
+
