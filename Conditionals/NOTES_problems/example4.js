8b0e0b8972d73fafe484d6a403e37737071c4843
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-27 07:12:41 +0530

    Add files via upload

---
+++ Conditionals/NOTES_problems/example4.js
@@ -0,0 +1,32 @@
+//predict the output:
+let a = 10;
+let b = 14;
+let c = 54;
+
+if(a>b)
+{
+    if(a>c)
+    {
+        console.log("a is greatest");
+    }
+    else
+    {
+        console.log("c is greatest");
+    }
+}
+else
+{
+    if(b>c)
+    {
+        console.log("b is greatest");
+    }
+    else
+    {
+        console.log("c is greatest");
+    }
+}
+
+/**
+ * Output:
+ * c is greatest
+ */
