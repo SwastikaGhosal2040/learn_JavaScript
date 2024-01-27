de2c861df4a8acd0f2277fb42e6085ef427d4f35
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-27 21:35:03 +0530

    Add files via upload

---
+++ Conditionals/NOTES_problems/example5.js
@@ -0,0 +1,13 @@
+//predict the output:
+var num = 10;
+
+//Condition 01: Any number that is completely divisible [remainder must be 0] by 2 then it is an even number.
+if(num % 2 == 0)
+{
+    console.log("The number given is an even number");
+}
+
+/**
+ * Output:
+ * The number given is an even number
+ */
