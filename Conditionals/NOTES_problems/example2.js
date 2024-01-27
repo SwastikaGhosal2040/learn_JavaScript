f98832cdff30996c3d524be156efde1bfd4a14a6
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-27 05:34:34 +0530

    Add files via upload

---
+++ Conditionals/NOTES_problems/example2.js
@@ -0,0 +1,15 @@
+//predict the output:
+age = 15;
+if(age<18)
+{
+    console.log("Sorry you are not eligible")
+}
+else
+{
+    console.log("Registered Successfully")
+}
+
+/**
+ * Output:
+ * Sorry you are not eligible
+ */
