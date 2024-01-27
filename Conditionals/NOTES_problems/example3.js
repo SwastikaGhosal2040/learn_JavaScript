3965526aec8e01ca0faf99c41bf0435b94d9ccec
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-27 06:02:18 +0530

    Add files via upload

---
+++ Conditionals/NOTES_problems/example3.js
@@ -0,0 +1,15 @@
+//predict the output:
+isAdmin = false;
+if(isAdmin)
+{
+    console.log("fetch user details")
+}
+else
+{
+    console.log("This operation is only for Admins")
+}
+
+/**
+ * Output:
+ * This operation is only for Admins
+ */
