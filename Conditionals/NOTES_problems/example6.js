d57b7b491c639088f1a9b6ad590d92fc3a40ed75
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-28 17:49:56 +0530

    Add files via upload

---
+++ Conditionals/NOTES_problems/example6.js
@@ -0,0 +1,17 @@
+//predict the output:
+var num = 17
+//condition 01: Any number that is completely divisible [remainder must be 0 ] by 2 then it is an even number.
+if(num % 2 == 0)
+{
+    console.log("The number given is an even number");
+}
+//condition 02: Any number that is not completely divisible [remainder must be 0] by 2 then it is an odd number.
+else
+{
+    console.log("The number given is an odd number");
+}
+
+/**
+ * Output:
+ * The number given is an odd number
+ */
