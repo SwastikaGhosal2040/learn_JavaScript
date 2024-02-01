ef51f29ae192ebbedef10c5b66d4d432fd7ca806
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-01 06:53:12 +0530

    Add files via upload

---
+++ Conditionals/NOTES_problems/b_example.js
@@ -0,0 +1,20 @@
+let color = "red";
+
+switch(color){
+    case "red":
+        console.log("The color is red.");
+    case "blue":
+        console.log("The color is blue.");
+        break;
+    case "green":
+        console.log("The color is green.");
+        break;
+    default:
+        console.log("The color is unknown.");
+}
+
+/**
+ * Output:
+ * The color is red.
+ * The color is blue.
+ */
