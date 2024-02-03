2bb8cc47fa1cb03d974a37cc184516a0f1fe4549
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-04 01:11:27 +0530

    Add files via upload

---
+++ Functions/A_functions.js
@@ -0,0 +1,15 @@
+//predict the output:
+function greet_students(){
+    console.log("Hi")
+    console.log("Welcome to 7 PM class")
+    console.log("Keep learning")
+}
+
+greet_students()
+
+/**
+ * Output:
+Hi
+Welcome to 7 PM class
+Keep learning
+ */
