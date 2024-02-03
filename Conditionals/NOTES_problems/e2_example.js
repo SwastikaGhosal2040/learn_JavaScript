09fe2bed81b2304942bf2ad5752859ce49bd943c
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-03 08:14:22 +0530

    Add files via upload

---
+++ Conditionals/NOTES_problems/e2_example.js
@@ -0,0 +1,13 @@
+//predict the output:
+let fruit = { name: 'Apple' };
+
+if(fruit.name === 'Apple'){
+    console.log('It\'s an apple!');
+}else{
+    console.log('Unknown fruit.');
+}
+
+/**
+ * Output:
+ * It's an apple!
+ */
