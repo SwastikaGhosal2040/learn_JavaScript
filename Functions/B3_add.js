94911a4c8bfece18b93f30b2e29128cd2a93a095
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-04 19:51:18 +0530

    Add files via upload

---
+++ Functions/B3_add.js
@@ -0,0 +1,12 @@
+//predict the output:
+function sum(a,b){
+    console.log("Inside the sum function")
+    return a + b
+}
+console.log(sum(5,6))
+
+/**
+ * Output:
+ * Inside the sum function
+ * 11
+ */
