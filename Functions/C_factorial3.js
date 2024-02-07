8a56817c7d35ec4bb5c34e0a1f47a232a4f6fbad
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-08 04:45:02 +0530

    Add files via upload

---
+++ Functions/C_factorial3.js
@@ -0,0 +1,19 @@
+//predict the output:
+function fact(num){
+    if(num==0 || num==1){
+        return 1
+    }
+    result = 1
+
+    for(i=1;i<=num;i++){
+        result = result * i
+    }
+    return result
+}
+
+console.log(fact(999))
+
+/**
+ * Output:
+ * Infinity
+ */
