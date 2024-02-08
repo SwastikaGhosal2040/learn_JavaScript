c98ba21af10d1e7ef4c7887bc4d1d74241c2c364
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-08 17:43:32 +0530

    Add files via upload

---
+++ Functions/D_Prob4a.js
@@ -0,0 +1,11 @@
+//predict the output:
+const hello_func = function hello(){
+    console.log("Hello my dear students")
+}
+console.log(hello_func())
+
+/**
+ * Output:
+ * Hello my dear students
+ * undefined
+ */
