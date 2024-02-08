31bea9d96e0c1bb40aaedaa4085dbbb24db1f5c6
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-08 21:08:42 +0530

    Add files via upload

---
+++ Functions/D_Prob4c.js
@@ -0,0 +1,11 @@
+//Amonymous function
+//predict the output:
+const hello_func = function(){
+    console.log("Hello my dear students")
+}
+hello_func()
+
+/**
+ * Output:
+ * Hello my dear students
+ */
