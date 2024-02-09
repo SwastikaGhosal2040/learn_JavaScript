0fe380027921f67dbe21759083f9ba49ddcfcb26
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-10 02:11:56 +0530

    Add files via upload

---
+++ Functions/E_Prob6d2.js
@@ -0,0 +1,17 @@
+function return_greet_fn(){
+    return function(){
+        console.log("Hello Students")
+    }
+}
+
+function func1(){
+    console.log("Hello from outer")
+    return function(){
+        console.log("Hello from inner")
+        return function(){
+            console.log("Hello from inner most")
+        }
+    }
+}
+
+func1()()
