1f8528d6e61a984a88548fc4d548e45d777c18e1
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-14 05:10:20 +0530

    Add files via upload

---
+++ Functions/F_Prob9_arrow2.js
@@ -0,0 +1,13 @@
+//predict the output:
+function add(a,b){
+    console.log("Inside the add function")
+    return a + b
+}
+
+console.log(add(5,6))
+
+/**
+ * Output:
+ * Inside the add function
+ * 11
+ */
