7205b30a4f162d6181dfaa8e7ceed1bebe0836c1
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-16 03:20:42 +0530

    Add files via upload

---
+++ Functions/F_Prob9_sum3_2.js
@@ -0,0 +1,13 @@
+//Predict the output:
+const sum_three = (a,b,c) => {
+    console.log("inside sum three")
+    return a + b + c
+}
+
+console.log(sum_three(4,5,6))
+
+/**
+ * Output:
+ * inside sum three
+ * 15
+ */
