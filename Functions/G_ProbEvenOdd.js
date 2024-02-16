681d7036f0a85ed7caed0f0ff88d8108d6b0f966
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-17 03:07:46 +0530

    Add files via upload

---
+++ Functions/G_ProbEvenOdd.js
@@ -0,0 +1,14 @@
+//Predict the output:
+function findEvenOrOdd(num){
+    if(num%2==0){
+        return "Even"
+    }else{
+        return "Odd"
+    }
+}
+console.log(findEvenOrOdd(23))
+
+/**
+ * Output:
+ * Odd
+ */
