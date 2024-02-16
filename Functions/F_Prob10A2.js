735758c5c86b565265305d49874175fd935ed186
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-17 02:38:42 +0530

    Add files via upload

---
+++ Functions/F_Prob10A2.js
@@ -0,0 +1,14 @@
+//Predict the output:
+greet = (name) => {
+    console.log("Hello " + name)
+}
+greet('Swastika')
+
+sum1 = (a,b) => {
+    return a + b
+}
+
+sum = (a,b) => a + b
+
+console.log(sum(5,6))
+console.log(sum1(2,8))
