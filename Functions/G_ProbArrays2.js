1092834d35802af3e78f737848863776fc72195a
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-18 04:19:08 +0530

    Add files via upload

---
+++ Functions/G_ProbArrays2.js
@@ -0,0 +1,12 @@
+//Predict the output:
+let arr_nums = [1,2,3,4,5]
+console.log(arr_nums)
+console.log(arr_nums[0])
+console.log(arr_nums[4])
+
+/**
+ * Output:
+ * [ 1, 2, 3, 4, 5 ]
+ * 1
+ * 5
+ */
