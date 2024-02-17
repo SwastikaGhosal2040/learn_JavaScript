c4f5d100c195c8ea44e66784e89eba4eaeda824b
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-17 17:12:59 +0530

    Add files via upload

---
+++ Functions/G_ProbEvenOddTernary.js
@@ -0,0 +1,23 @@
+//Predict the output:
+/** 
+findEvenOdd = (num) => {
+    if(num%2==0){
+        return "Even"
+    }else{
+        return "Odd"
+    }
+}
+console.log(findEvenOdd(23)) 
+
+Convert the above code into a single line of code.
+
+*/
+
+typ = (a) => (a % 2 == 0) ? "Even" : "Odd"
+
+console.log(typ(9))
+
+/**
+ * Output:
+ * Odd
+ */
