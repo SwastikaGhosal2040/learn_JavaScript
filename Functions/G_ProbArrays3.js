20ca15eb91e03b94fad8d385623cafe14b740099
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-18 11:15:15 +0530

    Add files via upload

---
+++ Functions/G_ProbArrays3.js
@@ -0,0 +1,12 @@
+//Predict the output:
+let arr_nums = [1,2,3,4,5]
+console.log(arr_nums)
+
+//Finding the length of the array
+console.log(arr_nums.length)
+
+/**
+ * Output:
+ * [ 1, 2, 3, 4, 5 ]
+ * 5
+ */
