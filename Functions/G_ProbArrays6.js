82065bbba76952c6ac5cb1216cc06f65e31c0d19
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-19 22:35:43 +0530

    Add files via upload

---
+++ Functions/G_ProbArrays6.js
@@ -0,0 +1,13 @@
+//Empty array
+//Predict the output:
+arr = Array(1,2,3,4,5)
+console.log(arr)
+
+empty_arr = Array(5)
+console.log(empty_arr)
+
+/**
+ * Output:
+ * [ 1, 2, 3, 4, 5 ]
+ * [ <5 empty items> ]
+ */
