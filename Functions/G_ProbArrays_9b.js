49b71183f3da3a65600d2c1b61485084a701c5c5
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-02-21 01:48:46 +0530

    Add files via upload

---
+++ Functions/G_ProbArrays_9b.js
@@ -0,0 +1,15 @@
+//Predict the output:
+/**
+ * How to iterate a loop
+ * 
+ * for loop
+ */
+
+arr = [1,2,3,4,5,6]
+
+console.log(arr)
+
+//For loop to print every element of the array
+for(i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
